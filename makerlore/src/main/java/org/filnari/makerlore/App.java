3216dcf38a835cbde0cbf613f432752b18e2921b
BuildTools <filnari.gmail.com>
2022-01-10 14:36:31 -0800

    first commit

---
+++ makerlore/src/main/java/org/filnari/makerlore/App.java
@@ -0,0 +1,18 @@
+package org.filnari.makerlore;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.inventory.ItemStack;
+public class App extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Enabling MakerLore");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabling MakerLore");
+    }
+    // Watch for item enchant
+
+    // case the type of item
+
+    // Build the lore on the item
+}

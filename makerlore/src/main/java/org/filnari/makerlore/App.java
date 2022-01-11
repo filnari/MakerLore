c02fbc15364aa3681d11ca90f7a3f02d7eec4be9
BuildTools <unconfigured@null.spigotmc.org>
2022-01-10 19:16:45 -0800

    Commit 2

--- makerlore/src/main/java/org/filnari/makerlore/App.java
+++ makerlore/src/main/java/org/filnari/makerlore/App.java
@@ -1,5 +1,12 @@
 package org.filnari.makerlore;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.chat.hover.content.Item;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
 public class App extends JavaPlugin {
     @Override
@@ -10,7 +17,21 @@ public class App extends JavaPlugin {
     public void onDisable() {
         getLogger().info("Disabling MakerLore");
     }
-    // Watch for item enchant
+
+    @EventHandler
+    public void Craft(CraftItemEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        String name = player.getName();
+        
+    }
+
+    @EventHandler
+    public void Prepare(PrepareItemCraftEvent event) {
+        // Player player = (Player) event.;
+        String name = player.getName();
+
+    }
+    // Watch for item craft or enchant
 
     // case the type of item
 
